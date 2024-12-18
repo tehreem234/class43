54eaadad814fe99fa8aa5d8e441fcd86e721dc85
tehreem234 <ifrahtehreem6@gmail.com>
2024-12-18 10:52:10 +0500

    example1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Example1 from './Example1/Example1'
+
+function App() {
+  return (
+    <div>
+      <h2>First Example</h2>
+      <Example1></Example1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Example1/Example1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Example1() {
+  return (
+    <div>
+      <h1>Welcome Back Saqlain</h1>
+    </div>
+  )
+}
+
+export default Example1
