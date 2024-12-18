ec6cff06228b416d12143cbe450c0b5a957e9655
tehreem234 <ifrahtehreem6@gmail.com>
2024-12-18 11:49:17 +0500

    example2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Welcome from "./Welcome/Welcome";
+
+function App(){
+  return(
+    <div>
+      <Welcome username="Tehreem" />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome(props) {
+  return (
+    <div>
+      <h1>Welcome back {props.username} </h1>
+    </div>
+  )
+}
+
+export default Welcome;
